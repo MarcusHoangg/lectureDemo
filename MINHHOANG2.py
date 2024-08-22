016e8a70238d5b8629517534b2a8a5091a27d975
MarcusHoangg <MINH.HOANG4@metropolia.fi>
2024-08-22 11:22:45 +0300

    SECOND Commit , Variables are defined

--- MINHHOANG2.py
+++ MINHHOANG2.py
@@ -1,3 +1,20 @@
+
+num1 = int(input("enter a number: "))
+num2 = float(input("enter another number: "))
+sum = num1 + num2
+print(sum)
+
+name = input("enter your name: ")
+school = input("enter your school: ")
+
+print("you are :", name, "and your school name  is :",  school)
+print(school)
+print(f"your name is,{name}, and your school name is {school}")
+floatVarialbe = 24.147108247120
+print(f"Your name is , {name},  and your floatVariable is , {floatVarialbe}")
+print(f"Your name is , {name},  and your floatVariable is , {floatVarialbe: 1.2f}")
+
+'''
 intVariable = 4
 floatVariable = 4.5
 stringVariable = "Software 1 is fun"
@@ -9,3 +26,13 @@ print(stringVariable)
 print(type(intVariable))
 print(type(floatVariable))
 print(type(stringVariable))
+
+#typecasing is fun
+intVariable = int(floatVariable)
+print("here is the int version of float variable" ,intVariable)
+''''''
+shareOfLoan = 500.50/3
+print(shareOfLoan)
+print(int(shareOfLoan))
+print(type(shareOfLoan))
+'''
